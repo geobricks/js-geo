ce4c8a2d8a3aa3105f609be191cfa62bcc97f531
guidofenix <guido.barbaglia@fao.org>
2014-07-22 11:59:22 +0200

    Require JS configuration with CDN's

--- js/main.js
+++ js/main.js
@@ -1,19 +1,45 @@
 require.config({
 
-    baseUrl: 'js',
+    baseUrl: 'js/libs',
 
     paths: {
-        'jquery': '//code.jquery.com/jquery-1.10.1.min.js'
+        'jquery': '//code.jquery.com/jquery-1.10.1.min',
+        'jquery.i18n': '//fenixapps.fao.org/repository/js/jquery/1.0.9/jquery.i18n.properties-min',
+        'bootstrap': '//netdna.bootstrapcdn.com/bootstrap/3.0.1/js/bootstrap.min',
+        'chosen': '//fenixapps.fao.org/repository/js/chosen/1.0.0/chosen.jquery.min',
+        'underscore': '//cdnjs.cloudflare.com/ajax/libs/underscore.js/1.6.0/underscore-min',
+        'highcharts': '//code.highcharts.com/highcharts'
     },
 
     shim: {
-        'i18n': {
+        'jquery.i18n': {
+            deps: ['jquery'],
+            exports: 'i18n',
+            init: function() {
+                $.i18n.properties({
+                    name: 'I18N',
+                    path: 'I18N/',
+                    mode: 'both',
+                    language: 'en'
+                });
+            }
+        },
+        'bootstrap': {
+            deps: ['jquery']
+        },
+        chosen: {
+            deps: ['jquery']
+        },
+        underscore: {
+            deps: ['jquery']
+        },
+        highcharts: {
             deps: ['jquery']
         }
     }
 
 });
 
-require(['jquery', 'js-geo'], function($, JSGEO) {
+require(['jquery', 'bootstrap', 'jquery.i18n', 'chosen', 'underscore', 'highcharts'], function($) {
     console.log('jQuery version:', $.fn.jquery);
 });
